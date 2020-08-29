66d8cf8ae6942feb5172567b21952fe20d521dfa
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2020-08-29 13:43:34 +0530

    second commit

---
+++ src/main/java/com/manikit/springboot/basics/springbootin10steps/Book.java
@@ -0,0 +1,36 @@
+package com.manikit.springboot.basics.springbootin10steps;
+
+public class Book {
+	
+	private Long id;
+	private String name;
+	private String author;
+	
+	public Book(Long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+	
+	 
+	
+	
+}
